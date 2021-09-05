f72b230a50da28a1ca1feded5ec692587ca476cc
Dave Beyer <dave.beyer@gmail.com>
2021-09-05 18:17:22 -0400

    move server-only helpers to gv-serverlib

--- typescript/helpers.js
+++ typescript/helpers.js
@@ -1,8 +1,7 @@
 "use strict";
 exports.__esModule = true;
-exports.GvApiMakeResponse = exports.GvApiIsInvalidRequest = exports.GvApiMakeRequest = exports.GvApiGenerateHash = void 0;
+exports.GvApiMakeRequest = exports.GvApiGenerateHash = void 0;
 var sha256 = require("crypto-js/sha256.js");
-var moment = require("moment");
 var consts_1 = require("./consts");
 // Examples:
 //   rqstType:    'import', 'export', ...
@@ -29,58 +28,3 @@ function GvApiMakeRequest(rqstType, rqstId, context, contextKey, rqstData) {
     return resp;
 }
 exports.GvApiMakeRequest = GvApiMakeRequest;
-// Returns GvApiError object on error, else null
-function GvApiIsInvalidRequest(rqstObj) {
-    if (rqstObj.version == null) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.MissingAPIVersion, "Missing API version field");
-    }
-    if (rqstObj.version !== consts_1.GvApiConstants.apiVersion) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.InvalidAPIVersion, "API version " + rqstObj.version + " is unsupported; currently on version " + consts_1.GvApiConstants.apiVersion);
-    }
-    var auth = rqstObj.auth;
-    //
-    // Authentication object check
-    //
-    if (auth == null) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.MissingAuthentication, "Missing authentication object");
-    }
-    if (auth.date == null) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.MissingAuthenticationDate, "Missing authentication date");
-    }
-    if (auth.hash == null) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.MissingAuthenticationHash, "Missing authentication hash");
-    }
-    //
-    // Date check
-    //
-    var date = moment(auth.date);
-    if (!date.isValid()) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.InvalidAuthDateFormat, "Unable to parse authentication date");
-    }
-    var now = moment();
-    var early = date.clone().subtract(consts_1.GvApiConstants.apiAuthTooEarly_s, "seconds");
-    // If the date plus 1 min is still before now, then it's too early
-    if (now.isBefore(early)) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.EarlyAuthDate, "Authentication date is too early");
-    }
-    var late = date.clone().add(consts_1.GvApiConstants.apiAuthTooLate_s, "seconds");
-    // If the date minus 1 min is still after now, then it's too stale
-    if (now.isAfter(late)) {
-        return new consts_1.GvApiError(consts_1.GvApiErrorCodes.StaleAuthDate, "Authentication has expired");
-    }
-    // Following this, still need to check:
-    //   auth.date is advancing from prior API call
-    //   auth.hash matches based on context's api key
-    return null;
-}
-exports.GvApiIsInvalidRequest = GvApiIsInvalidRequest;
-function GvApiMakeResponse(rqstType, rqstId, respData, errObj) {
-    return {
-        version: consts_1.GvApiConstants.apiVersion,
-        request: rqstType,
-        requestId: rqstId,
-        data: respData,
-        error: errObj != null ? errObj : null
-    };
-}
-exports.GvApiMakeResponse = GvApiMakeResponse;
--- typescript/helpers.ts
+++ typescript/helpers.ts
@@ -1,11 +1,7 @@
-import * as sha256       from "crypto-js/sha256.js";
-import * as moment       from "moment";
+import * as sha256 from "crypto-js/sha256.js";
 
-import { GvApiConstants,
-         GvApiErrorCodes,
-         GvApiRequest,
-         GvApiAuth,
-         GvApiError } from "./consts";
+import { GvApiRequest,
+         GvApiAuth } from "./consts";
 
 // Examples:
 //   rqstType:    'import', 'export', ...
@@ -14,9 +10,11 @@ import { GvApiConstants,
 //   contextKey:  secret apiKey for this account
 //   data:        Relevant data object
 
+
 export function GvApiGenerateHash (context: string, contextKey: string, date: string) {
     return sha256(context.trim().toLowerCase() + contextKey.trim() + date.trim()).toString();
 }
+
 
 export function GvApiMakeRequest(rqstType: string, rqstId: string,
                                context: string, contextKey: string, rqstData: any) {
@@ -36,88 +34,3 @@ export function GvApiMakeRequest(rqstType: string, rqstId: string,
 
     return resp;
 }
-
-// Returns GvApiError object on error, else null
-export function GvApiIsInvalidRequest(rqstObj: any): GvApiError {
-
-    if (rqstObj.version == null) {
-        return new GvApiError(GvApiErrorCodes.MissingAPIVersion,
-                              "Missing API version field");
-    }
-
-    if (rqstObj.version !== GvApiConstants.apiVersion) {
-        return new GvApiError(GvApiErrorCodes.InvalidAPIVersion,
-                              `API version ${rqstObj.version} is unsupported; currently on version ${GvApiConstants.apiVersion}`);
-    }
-
-    let auth = rqstObj.auth;
-
-    //
-    // Authentication object check
-    //
-
-    if (auth == null) {
-        return new GvApiError(GvApiErrorCodes.MissingAuthentication,
-                            "Missing authentication object");
-    }
-
-    if (auth.date == null) {
-        return new GvApiError(GvApiErrorCodes.MissingAuthenticationDate,
-                            "Missing authentication date");
-    }
-
-    if (auth.hash == null) {
-        return new GvApiError(GvApiErrorCodes.MissingAuthenticationHash,
-                            "Missing authentication hash");
-    }
-
-    //
-    // Date check
-    //
-
-    let date = moment(auth.date);
-
-    if (! date.isValid()) {
-        return new GvApiError(GvApiErrorCodes.InvalidAuthDateFormat,
-                            "Unable to parse authentication date");
-    }
-
-    let now = moment();
-
-    let early = date.clone().subtract(GvApiConstants.apiAuthTooEarly_s, "seconds");
-
-    // If the date plus 1 min is still before now, then it's too early
-    if (now.isBefore(early)) {
-        return new GvApiError(GvApiErrorCodes.EarlyAuthDate,
-                            "Authentication date is too early");
-    }
-
-    let late = date.clone().add(GvApiConstants.apiAuthTooLate_s, "seconds");
-
-    // If the date minus 1 min is still after now, then it's too stale
-    if (now.isAfter(late)) {
-        return new GvApiError(GvApiErrorCodes.StaleAuthDate,
-                            "Authentication has expired");
-    }
-
-    // Following this, still need to check:
-    //   auth.date is advancing from prior API call
-    //   auth.hash matches based on context's api key
-
-    return null;
-}
-
-export function GvApiMakeResponse(rqstType: string, rqstId: string, respData: any, errObj?: GvApiError) {
-    return {
-        version   : GvApiConstants.apiVersion,
-
-        request   : rqstType,
-        requestId : rqstId,
-
-        data      : respData,
-        error     : errObj != null ? errObj : null
-    };
-}
-
-
-
